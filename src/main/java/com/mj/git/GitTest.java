09d9428576925977649b37160d0a9dd47f0a49f8
mj <mj@123.com>
2021-05-11 11:42:04 +0800

    master commit

--- src/main/java/com/mj/git/GitTest.java
+++ src/main/java/com/mj/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello world!2222");
         System.out.println("hello world!3333");
         System.out.println("hello world!hot fix");
+        System.out.println("hello world!hot fix");
     }
 }
